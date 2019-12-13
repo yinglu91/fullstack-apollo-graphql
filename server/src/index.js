eb384ef09ff47b293c60953b047979f28e1f256e
Ying Lu <yinglu91@gmail.com>
2019-12-12 22:24:04 -0500

    added rest data source

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,33 @@ const {
     ApolloServer
 } = require('apollo-server');
 const typeDefs = require('./schema');
+const {
+    createStore
+} = require('./utils');
 
+const resolvers = require('./resolvers');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+// Apollo Server will automatically add the launchAPI and userAPI to our resolvers' context so we can easily call them.
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+
+    resolvers,
+
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({
+            store
+        })
+    })
 });
 
 server.listen().then(({
     url
 }) => {
-    console.log(` Server ready at ${url}`)
+    console.log(`🚀 Server ready at ${url}`);
 });
